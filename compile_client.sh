4d21f79636d02811a286bc2a9ffafc05e9c410e7
Dan Larsen <dan@techba.se>
2014-10-26 14:24:20 +0100

    Auto compile the client

---
+++ compile_client.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "(function() {\n" > browser/client.js;
+cat lib/browser/function.bind.js >> browser/client.js;
+cat lib/browser/xhr.js >> browser/client.js;
+cat lib/browser/poll.js >> browser/client.js;
+cat lib/browser/sockit.js >> browser/client.js;
+echo "\nwindow.SockIt = SockIt;\n})();\n" >> browser/client.js;
