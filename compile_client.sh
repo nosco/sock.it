99261e1dc54fff4e7bdeda31e0b030188fdaa452
Dan Larsen <dan@techba.se>
2014-12-30 12:21:45 +0100

    Changed the uglifying to be done in the compile script

--- compile_client.sh
+++ compile_client.sh
@@ -6,3 +6,4 @@ cat lib/browser/xhr.js >> browser/client.js;
 cat lib/browser/poll.js >> browser/client.js;
 cat lib/browser/sockit.js >> browser/client.js;
 echo "\nwindow.SockIt = SockIt;\n})();\n" >> browser/client.js;
+./node_modules/.bin/uglify -s browser/client.js -o browser/client.min.js
