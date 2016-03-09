cc301d521d5d984047c9101f843eedd59cc1f7a9
Elias G. Schneevoigt <eliasgs@gmail.com>
2016-03-09 11:17:39 +0100

    Make echo interpret escape characters

--- compile_client.sh
+++ compile_client.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "(function() {\n" > browser/client.js;
+echo -e "(function() {\n" > browser/client.js;
 cat lib/browser/debug.js >> browser/client.js;
 cat lib/browser/function.bind.js >> browser/client.js;
 cat lib/browser/xhr.js >> browser/client.js;
 cat lib/browser/poll.js >> browser/client.js;
 cat lib/browser/sockit.js >> browser/client.js;
-echo "\nwindow.SockIt = SockIt;\n})();\n" >> browser/client.js;
+echo -e "\nwindow.SockIt = SockIt;\n})();\n" >> browser/client.js;
 uglify -s browser/client.js -o browser/client.min.js
